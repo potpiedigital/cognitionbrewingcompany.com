25e5b1376322b6a1a824c35978510b49e9472bb4
Mark Hayes <mark.m.hayes1@gmail.com>
2020-02-13 20:41:44 -0500

    fat arrows and dynamic pages

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,18 @@
 // import Header from "../comps/Header";
-import Layout from "../components/MyLayout";
+import Layout from "../comps/MyLayout";
 
-export default function About() {
-  return (
-    <Layout>
-      <p>This is the about page</p>
-    </Layout>
-  );
-}
+// export default function About() {
+//   return (
+//     <Layout>
+//       <p>This is the about page</p>
+//     </Layout>
+//   );
+// }
+
+const About = () => (
+  <Layout>
+    <p>This is the about page using fat arrow</p>
+  </Layout>
+);
+
+export default About;
