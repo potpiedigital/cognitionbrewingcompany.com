11ea046f118b93f24ed1a1890f3f80614711e8e5
Matt Hayes <matt@mysterycommand.com>
2020-06-02 21:57:19 -0400

    feat: use api items for listing items

--- components/On-tap.js
+++ components/On-tap.js
@@ -1,110 +1,45 @@
 import ListingItem from "./Listing-item";
 
-const OnTap = () => {
-  return (
-    <div className="on-tap">
-      <h4>On Tap Now</h4>
-      <div className="flow">
+const OnTap = ({ items }) => (
+  <div className="on-tap">
+    <h4>On Tap Now</h4>
+    <div className="flow">
+      {items.map((item) => (
         <ListingItem
-          title="Gnome Wrecker belgian style Pale"
-          desc="Pale Ale - Belgian"
-          numbers="5.9% ABV - 37 IBU"
-          rating="beer rating img"
+          key={item.id}
+          title={item.name}
+          desc={item.style}
+          numbers={`${item.abv}% ABV - ${item.ibu} IBU`}
+          rating={parseFloat(item.rating, 10).toFixed(2)}
         />
-        <ListingItem
-          title="Deep Scream Cream Ale"
-          desc="Cream Ale"
-          numbers="5.2% ABV - 20 IBU"
-          rating="beer rating img"
-        />
-        <ListingItem
-          title="Oblivion Milk Stour"
-          desc="Stout Milk/Sweet"
-          numbers="5% ABV - 30 IBM"
-          rating="beer rating img"
-        />
-        <ListingItem
-          title="Octopus Was Very Scary"
-          desc="Stout - Other"
-          numbers="7.7% ABV - 17IBU"
-          rating="beer rating img"
-        />
-        <ListingItem
-          title="Neurosis Pils"
-          desc="Pilsner - German"
-          numbers="5.9% ABV - 55 IBU"
-          rating="beer rating img"
-        />
-        <ListingItem
-          title="Languish"
-          desc="Pale Ale - American"
-          numbers="5.5% ABV - 45 IBU"
-          rating="beer rating img"
-        />
-        <ListingItem
-          title="Black Mass Porter"
-          desc="Porter - Other"
-          numbers="6.3% ABV - 30 IBU"
-          rating="beer rating img"
-        />
-        <ListingItem
-          title="Gnome Wrecker belgian style Pale"
-          desc="Pale Ale - Belgian"
-          numbers="5.9% ABV - 37 IBU"
-          rating="beer rating img"
-        />
-        <ListingItem
-          title="Deep Scream Cream Ale"
-          desc="Cream Ale"
-          numbers="5.2% ABV - 20 IBU"
-          rating="beer rating img"
-        />
-        <ListingItem
-          title="Oblivion Milk Stour"
-          desc="Stout Milk/Sweet"
-          numbers="5% ABV - 30 IBM"
-          rating="beer rating img"
-        />
-        <ListingItem
-          title="Octopus Was Very Scary"
-          desc="Stout - Other"
-          numbers="7.7% ABV - 17IBU"
-          rating="beer rating img"
-        />
-        <ListingItem
-          title="Neurosis Pils"
-          desc="Pilsner - German"
-          numbers="5.9% ABV - 55 IBU"
-          rating="beer rating img"
-        />
-      </div>
-      <style jsx>{`
-        .on-tap {
-          grid-column: 2 / 15;
-          grid-row: 4 / 8;
-          margin-top: 2rem;
-        }
-
-        h4 {
-          padding-bottom: 1.5rem;
-        }
-
-        .flow {
-          display: flex;
-          flex-wrap: wrap;
-          justify-content: start;
-          height: 90%;
-        }
-        @media screen and (max-width: 768px) {
-          .on-tap {
-            grid-column: 1 / 8;
-            grid-row: 2 / 4;
-            margin-left: 2rem;
-          }
-        }
-      `}</style>
+      ))}
     </div>
-  );
-};
+    <style jsx>{`
+      .on-tap {
+        grid-column: 2 / 15;
+        grid-row: 4 / 8;
+        margin-top: 2rem;
+      }
+
+      h4 {
+        padding-bottom: 1.5rem;
+      }
+
+      .flow {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: start;
+        height: 90%;
+      }
+      @media screen and (max-width: 768px) {
+        .on-tap {
+          grid-column: 1 / 8;
+          grid-row: 2 / 4;
+          margin-left: 2rem;
+        }
+      }
+    `}</style>
+  </div>
+);
 
 export default OnTap;
